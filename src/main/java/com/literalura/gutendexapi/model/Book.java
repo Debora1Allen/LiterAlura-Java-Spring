ae0a5b1808ade448d1bad16e23c0b259d7151216
Silva <deboraaaline21@gmail.com>
2024-07-16 15:57:47 -0300

    feat: Cria models para livro e autor

---
+++ src/main/java/com/literalura/gutendexapi/model/Book.java
@@ -0,0 +1,24 @@
+package com.literalura.gutendexapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String languages;
+    private Integer downloads;
+}
